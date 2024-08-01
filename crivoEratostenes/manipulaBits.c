0b927ce32393faeee72d37f15c3f11e9fdcf0ce6
Ana Paula <164682309+anapaulacrippa@users.noreply.github.com>
2024-08-01 11:02:59 -0300

    update manipulaBits.c

--- crivoEratostenes/manipulaBits.c
+++ crivoEratostenes/manipulaBits.c
@@ -1,14 +1,5 @@
 /*
-TRABALHO 1 - PROGRAMAÇÃO DE SISTEMAS / 14138 - TURMA 01
-RA 137304 - Ana Paula Loureiro Crippa
-
-Usando o Crivo de Eratóstenes construa uma tabela de consulta (lockup
-table) que permita dizer se um número inteiro positivo pertencente ao intervalo [2, 6400] é
-primo ou não.
-(1)Deve-se fazer uma função que seja capaz de montar essa tabela com um esforço
-computacional e uso de memória mínimos e;
-(2)Deve-se fazer uma função que realize consultas nessa tabela para informar se um
-determinado valor é primo ou não com custo mínimo de processamento.
+TRABALHO 1 - PROGRAMAÇÃO DE SISTEMAS / 14138
 */
 
 #include <stdio.h>
@@ -36,15 +27,13 @@ int GetBitn(int num, int n) {
     return ((num & (1 << n)) >> n);
 }
 
-int InvBitn(int num, int n)
-{
+int InvBitn(int num, int n) {
     // Inverte o valor (1 ou 0) do n-ésimo bit de num.
     // 0 =< n <= 31
     return (num ^ (1 << n));
 }
 
-int ChangeBitn(int num, int n, int b)
-{
+int ChangeBitn(int num, int n, int b) {
     // Modifica o valor do n-ésimo bit de num pelo valor b (0 ou 1).
     // 0 =< n <= 31
     int mask = 1 << n;
